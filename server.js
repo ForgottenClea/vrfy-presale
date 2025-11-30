243aa382df8fd37f76ba64d281cac374ed086e34
Thomas Pace <macbookpro@Thomass-MacBook-Pro.local>
2025-11-30 12:09:04 -0500

    Fix Railway deployment: bind to 0.0.0.0, add Procfile

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`VRFY Presale running on port ${PORT}`);
 });
